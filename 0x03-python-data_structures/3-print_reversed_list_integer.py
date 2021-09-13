8919db593784b6f76dad324737b86b6f3759507d
yazgiraldoa <edygiraldo.97@gmail.com>
2021-09-13 14:57:49 -0500

    fix function that prints int from a list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for n in range((len(my_list) - 1), -1, -1):
-        print("{:d}".format(my_list[n]))
+    if my_list:
+        for n in range((len(my_list) - 1), -1, -1):
+            print("{:d}".format(my_list[n]))
