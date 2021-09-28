f944b306c99d79f6f2b1849669a5797c084446b3
yazgiraldoa <edygiraldo.97@gmail.com>
2021-09-28 17:30:28 -0500

    add empty class that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defining an empty class.
+"""
+
+
+class Square:
+    """
+    Empty class that defines a square.
+    Attributes:
+        This class doesn't have attributes.
+    """
+    pass
