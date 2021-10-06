0c2795b3555ad2d3e21ab61db189907acc6ade40
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-06 17:49:08 -0500

    add function that prints a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Function that prints a square with the character #.
+"""
+
+
+def print_square(size):
+    """
+    Function that prints a square with the character #.
+    Size must be an integer and > 0
+    """
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print()
+
+    i = 1
+    while i <= size:
+        j = 1
+        while j <= size:
+            print("#", end='')
+            j += 1
+        print()
+        i += 1
