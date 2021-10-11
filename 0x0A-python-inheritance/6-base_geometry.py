c3566c48c9237c8b0477907a54f198c3e87d324a
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-11 18:16:53 -0500

    Defining area of a class Base Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defining area of class Base Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class Base Geometry with public instance method area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
