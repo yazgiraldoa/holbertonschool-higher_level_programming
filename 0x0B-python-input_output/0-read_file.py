517608c3594d5d5e4d15abe9fcbfe2227d7770c4
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-12 17:03:37 -0500

    add function that reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     """Function that reads a file and prints its content"""
     with open(filename, encoding='utf-8') as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end='')
