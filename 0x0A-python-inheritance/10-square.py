377edc16dadae543191952f9e8d17980df387aa8
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-11 22:03:11 -0500

    add Square inherited class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,54 +2,17 @@
 """
 Add Square inherited class
 """
-
-
-class BaseGeometry:
-    """
-    Class Base Geometry with public instance method area
-    and integer validation
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """
-        Class Rectangle inherited from Base Geometry
-        Attributes:
-            width(int): width of a rectangle - private attribute.
-            height(int): height of a rectangle - private attribute.
-    """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-            Class Square inherited from Rectangle
-            Attributes:
-                size(int): size of a square - private attribute.
+    Class Square inherited from Rectangle
+    Attributes:
+        size(int): size of a square - private attribute.
     """
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
+        Rectangle.__init__(self, size, size)
