3d7ca36a399f97178f25ebb670628a4c298ab264
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-11 22:01:40 -0500

    add str and area methods to inherited class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,21 +2,7 @@
 """
 Add str and area methods to inherited class
 """
-
-
-class BaseGeometry:
-    """
-    Class Base Geometry with public instance method area
-    and integer validation
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
