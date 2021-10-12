2a35979acc61d6fda34af8d980752bf52e919e5c
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-12 16:53:11 -0500

    add fucntion that returns object form Json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that  returns an object (Python data structure)
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Function that returns an object represented by JSON str"""
+    return json.loads(my_str)
