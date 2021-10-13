043350726890ff0b482687625d824b8958b56c17
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-12 19:49:57 -0500

    add method dict representation with some attr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Class Student with public method to_json
+"""
+
+
+class Student:
+    """
+    Class Student that has a method to print dict
+    representation of a Student instance.
+    Attributes:
+        first_name(str): student's first name
+        last_name(str): student's last name
+        age(int): student's age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        all_attr = self.__dict__.copy()
+
+        if isinstance(attrs, list):
+            for key in self.__dict__.keys():
+                if key not in attrs:
+                    del all_attr[key]
+        return all_attr
