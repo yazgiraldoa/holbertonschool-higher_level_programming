93bd240c19024b5bc65ee021c03a4014bbf401d3
yazgiraldoa <edygiraldo.97@gmail.com>
2021-10-18 15:25:10 -0500

    update documentation of methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,18 +23,24 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Static method that returns the JSON string
+        representation of a list of dictionaries"""
         if not list_dictionaries or list_dictionaries is None:
             return {}
         return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
+        """Static method that returns the list of
+        the JSON string representation json_string"""
         if not json_string or json_string is None:
             return []
         return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """Class method that writes the JSON string
+            representation of list_objs to a file"""
         list_dicts = []
         if list_objs is not None:
             for obj in list_objs:
@@ -49,12 +55,15 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """Class method that returns an instance
+        with all attributes already set"""
         r1 = cls.__mro__[0](1, 2, 1, 1)
         r1.update(**dictionary)
         return r1
 
     @classmethod
     def load_from_file(cls):
+        """Class method that returns a list of instances"""
         obj_list = []
         try:
             with open(f"{cls.__name__}.json", encoding='utf-8') as f:
