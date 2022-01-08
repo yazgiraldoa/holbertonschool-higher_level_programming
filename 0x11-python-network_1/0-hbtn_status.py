14b26422b09dd3be14d18ffe82662d2236c9a213
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-08 16:08:59 -0500

    add validation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
-import urllib.request
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+from urllib import request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode('utf8'))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf8'))
