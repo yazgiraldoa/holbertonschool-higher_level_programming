c38252262aefd29e48cbd799e4278509ba80cff2
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-08 22:38:11 -0500

    add script that handles request errors

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to
+the URL and displays the body of the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code == 200:
+        print(r.text)
+    else:
+        print("Error code:", r.status_code)
