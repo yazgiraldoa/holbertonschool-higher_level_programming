dcb078e327241994477071237302008af80d8502
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-08 22:13:43 -0500

    add script that gets headers value - requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a URL,
+ sends a request to the URL and displays the value of
+ the variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    try:
+        print(r.headers['X-Request-Id'])
+    except Exception as e:
+        pass
