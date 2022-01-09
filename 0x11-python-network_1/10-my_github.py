e0f02779493b6a4894424c897554ca70218097b3
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-09 14:23:44 -0500

    add script that connects to github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(username, pwd))
+    try:
+        json = r.json()
+        print(json["id"])
+    except KeyError:
+        print("None")
