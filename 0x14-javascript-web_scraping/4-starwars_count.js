b952e0a93b140cb917daf21b9f35c1a4f81cd3f1
yazgiraldoa <edygiraldo.97@gmail.com>
2022-02-01 14:24:28 -0500

    add script that fetches an API and finds a character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,19 @@
 // Script that prints the number of movies where the character “Wedge Antilles” is present.
 const request = require('request');
 const { argv } = require('process');
-const url = argv[2] + '?id=18';
+const url = argv[2];
+let count = 0;
 
 request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
     return;
   }
-  const json = JSON.parse(body);
-  console.log(json.count);
+  const json = JSON.parse(body).results;
+  for (const movie of json) {
+    for (const person of movie.characters) {
+      if (person.includes('18')) { count += 1; }
+    }
+  }
+  console.log(count);
 });
